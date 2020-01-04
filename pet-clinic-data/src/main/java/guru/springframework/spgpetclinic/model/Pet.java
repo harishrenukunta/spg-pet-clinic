1072bc7f2664e25c870dbfbfc2be78e63df7ee43
harishrenukunta <harish_r9@yahoo.co.uk>
2020-01-04 20:48:47 +0000

    Add BaseEntity and id to all necessary classes. close #2

--- pet-clinic-data/src/main/java/guru/springframework/spgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/spgpetclinic/model/Pet.java
@@ -1,4 +1,24 @@
 package guru.springframework.spgpetclinic.model;
 
-public class Pet {
+public class Pet extends BaseEntity {
+
+    private Owner owner;
+    private PetType petType;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
 }
+
